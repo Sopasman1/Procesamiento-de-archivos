208f2fbc7f6b20335b76afca4d3b1894200f9a56
Jesus Alberto Sandoval Peña <Jezuz33@outlook.com>
2025-06-08 22:53:56 -0600

    code

--- Procesamiento de archivos/Form1.Designer.cs
+++ Procesamiento de archivos/Form1.Designer.cs
@@ -28,12 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tabcontrol = new TabControl();
+            tabPage1 = new TabPage();
+            dataGridViewCsv = new DataGridView();
+            btnLoadcsv = new Button();
+            btnBrowsecsv = new Button();
+            tabPage2 = new TabPage();
+            dataGridViewXml = new DataGridView();
+            btnLoadxml = new Button();
+            btnBrowsexml = new Button();
+            tabPage3 = new TabPage();
+            dataGridViewJson = new DataGridView();
+            btnLoadjson = new Button();
+            btnBrowsejson = new Button();
+            tabcontrol.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCsv).BeginInit();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewXml).BeginInit();
+            tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewJson).BeginInit();
+            SuspendLayout();
+            // 
+            // tabcontrol
+            // 
+            tabcontrol.Controls.Add(tabPage1);
+            tabcontrol.Controls.Add(tabPage2);
+            tabcontrol.Controls.Add(tabPage3);
+            tabcontrol.Location = new Point(12, 12);
+            tabcontrol.Name = "tabcontrol";
+            tabcontrol.SelectedIndex = 0;
+            tabcontrol.Size = new Size(776, 438);
+            tabcontrol.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(dataGridViewCsv);
+            tabPage1.Controls.Add(btnLoadcsv);
+            tabPage1.Controls.Add(btnBrowsecsv);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(768, 410);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "CSV";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewCsv
+            // 
+            dataGridViewCsv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCsv.Location = new Point(6, 44);
+            dataGridViewCsv.Name = "dataGridViewCsv";
+            dataGridViewCsv.Size = new Size(756, 358);
+            dataGridViewCsv.TabIndex = 2;
+            // 
+            // btnLoadcsv
+            // 
+            btnLoadcsv.Location = new Point(96, 15);
+            btnLoadcsv.Name = "btnLoadcsv";
+            btnLoadcsv.Size = new Size(75, 23);
+            btnLoadcsv.TabIndex = 1;
+            btnLoadcsv.Text = "Load";
+            btnLoadcsv.UseVisualStyleBackColor = true;
+            btnLoadcsv.Click += btnLoadcsv_Click;
+            // 
+            // btnBrowsecsv
+            // 
+            btnBrowsecsv.Location = new Point(6, 15);
+            btnBrowsecsv.Name = "btnBrowsecsv";
+            btnBrowsecsv.Size = new Size(75, 23);
+            btnBrowsecsv.TabIndex = 0;
+            btnBrowsecsv.Text = "Browse";
+            btnBrowsecsv.UseVisualStyleBackColor = true;
+            btnBrowsecsv.Click += btnBrowsecsv_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(dataGridViewXml);
+            tabPage2.Controls.Add(btnLoadxml);
+            tabPage2.Controls.Add(btnBrowsexml);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(768, 410);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "XML";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewXml
+            // 
+            dataGridViewXml.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewXml.Location = new Point(6, 35);
+            dataGridViewXml.Name = "dataGridViewXml";
+            dataGridViewXml.Size = new Size(756, 367);
+            dataGridViewXml.TabIndex = 2;
+            // 
+            // btnLoadxml
+            // 
+            btnLoadxml.Location = new Point(87, 6);
+            btnLoadxml.Name = "btnLoadxml";
+            btnLoadxml.Size = new Size(75, 23);
+            btnLoadxml.TabIndex = 1;
+            btnLoadxml.Text = "Load";
+            btnLoadxml.UseVisualStyleBackColor = true;
+            btnLoadxml.Click += btnLoadxml_Click;
+            // 
+            // btnBrowsexml
+            // 
+            btnBrowsexml.Location = new Point(6, 6);
+            btnBrowsexml.Name = "btnBrowsexml";
+            btnBrowsexml.Size = new Size(75, 23);
+            btnBrowsexml.TabIndex = 0;
+            btnBrowsexml.Text = "Browse";
+            btnBrowsexml.UseVisualStyleBackColor = true;
+            btnBrowsexml.Click += btnBrowsexml_Click;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(dataGridViewJson);
+            tabPage3.Controls.Add(btnLoadjson);
+            tabPage3.Controls.Add(btnBrowsejson);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(768, 410);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "JSON";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewJson
+            // 
+            dataGridViewJson.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewJson.Location = new Point(6, 35);
+            dataGridViewJson.Name = "dataGridViewJson";
+            dataGridViewJson.Size = new Size(756, 369);
+            dataGridViewJson.TabIndex = 2;
+            // 
+            // btnLoadjson
+            // 
+            btnLoadjson.Location = new Point(87, 6);
+            btnLoadjson.Name = "btnLoadjson";
+            btnLoadjson.Size = new Size(75, 23);
+            btnLoadjson.TabIndex = 1;
+            btnLoadjson.Text = "Load";
+            btnLoadjson.UseVisualStyleBackColor = true;
+            btnLoadjson.Click += btnLoadjson_Click;
+            // 
+            // btnBrowsejson
+            // 
+            btnBrowsejson.Location = new Point(6, 6);
+            btnBrowsejson.Name = "btnBrowsejson";
+            btnBrowsejson.Size = new Size(75, 23);
+            btnBrowsejson.TabIndex = 0;
+            btnBrowsejson.Text = "Browse";
+            btnBrowsejson.UseVisualStyleBackColor = true;
+            btnBrowsejson.Click += btnBrowsejson_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabcontrol);
+            Name = "Form1";
+            Text = "Form1";
+            tabcontrol.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCsv).EndInit();
+            tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewXml).EndInit();
+            tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewJson).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabcontrol;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private DataGridView dataGridViewCsv;
+        private Button btnLoadcsv;
+        private Button btnBrowsecsv;
+        private DataGridView dataGridViewXml;
+        private Button btnLoadxml;
+        private Button btnBrowsexml;
+        private DataGridView dataGridViewJson;
+        private Button btnLoadjson;
+        private Button btnBrowsejson;
     }
 }
--- Procesamiento de archivos/Form1.cs
+++ Procesamiento de archivos/Form1.cs
@@ -1,10 +1,258 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Xml;
 namespace Procesamiento_de_archivos
 {
     public partial class Form1 : Form
     {
+        private string currentCsvPath = "";
+        private string currentXmlPath = "";
+        private string currentJsonPath = "";
         public Form1()
         {
             InitializeComponent();
+
+
         }
+
+        private void btnBrowsecsv_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+                ofd.Title = "Select CSV File";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    currentCsvPath = ofd.FileName;
+                    btnLoadcsv.Enabled = true;
+                }
+            }
+        }
+
+        private void btnLoadcsv_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentCsvPath))
+            {
+                MessageBox.Show("Please select a file first", "Warning",
+                             MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                dataGridViewCsv.DataSource = LoadCsvData(currentCsvPath);
+                MessageBox.Show("CSV loaded successfully!", "Success",
+                              MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading CSV: {ex.Message}", "Error",
+                             MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private DataTable LoadCsvData(string filePath)
+        {
+            DataTable dt = new DataTable();
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string[] headers = sr.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    dt.Columns.Add(header.Trim());
+                }
+
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = sr.ReadLine().Split(',');
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < headers.Length && i < rows.Length; i++)
+                    {
+                        dr[i] = rows[i].Trim();
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            return dt;
+        }
+
+        private void btnBrowsexml_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "XML Files (*.xml)|*.xml|All Files (*.*)|*.*";
+                ofd.Title = "Select XML File";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    currentXmlPath = ofd.FileName;
+                    btnLoadxml.Enabled = true;
+                }
+            }
+        }
+
+        private void btnLoadxml_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentXmlPath))
+            {
+                MessageBox.Show("Please select a file first", "Warning",
+                             MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                dataGridViewXml.DataSource = LoadXmlData(currentXmlPath);
+                MessageBox.Show("XML loaded successfully!", "Success",
+                              MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading XML: {ex.Message}", "Error",
+                             MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private DataTable LoadXmlData(string filePath)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Node Name");
+            dt.Columns.Add("Node Value");
+            dt.Columns.Add("Attributes");
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
+
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                DataRow row = dt.NewRow();
+                row["Node Name"] = node.Name;
+                row["Node Value"] = node.InnerText;
+
+                if (node.Attributes != null && node.Attributes.Count > 0)
+                {
+                    string attributes = "";
+                    foreach (XmlAttribute attr in node.Attributes)
+                    {
+                        attributes += $"{attr.Name}={attr.Value}; ";
+                    }
+                    row["Attributes"] = attributes.Trim();
+                }
+
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        }
+
+        private void btnBrowsejson_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "JSON Files (*.json)|*.json|All Files (*.*)|*.*";
+                ofd.Title = "Select JSON File";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    currentJsonPath = ofd.FileName;
+                    btnLoadjson.Enabled = true;
+                }
+            }
+
+        }
+
+        private void btnLoadjson_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentJsonPath))
+            {
+                MessageBox.Show("Please select a JSON file first", "Warning",
+                             MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                dataGridViewJson.DataSource = LoadJsonData(currentJsonPath);
+                MessageBox.Show("JSON file loaded successfully", "Success",
+                              MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading JSON: {ex.Message}", "Error",
+                             MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private DataTable LoadJsonData(string filePath)
+        {
+            string jsonContent = File.ReadAllText(filePath);
+
+            // Manejar tanto arrays como objetos individuales
+            if (jsonContent.Trim().StartsWith("["))
+            {
+                // Es un array JSON
+                JArray jsonArray = JArray.Parse(jsonContent);
+                return JsonArrayToDataTable(jsonArray);
+            }
+            else
+            {
+                // Es un objeto JSON individual
+                JObject jsonObject = JObject.Parse(jsonContent);
+                return JsonObjectToDataTable(jsonObject);
+            }
+        }
+
+        private DataTable JsonArrayToDataTable(JArray jsonArray)
+        {
+            DataTable dt = new DataTable();
+
+            // Crear columnas basadas en las propiedades del primer elemento
+            if (jsonArray.Count > 0)
+            {
+                JObject firstItem = (JObject)jsonArray[0];
+                foreach (JProperty prop in firstItem.Properties())
+                {
+                    dt.Columns.Add(prop.Name);
+                }
+            }
+
+            // Llenar datos
+            foreach (JObject item in jsonArray)
+            {
+                DataRow row = dt.NewRow();
+                foreach (JProperty prop in item.Properties())
+                {
+                    row[prop.Name] = prop.Value.ToString();
+                }
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        }
+
+        private DataTable JsonObjectToDataTable(JObject jsonObject)
+        {
+            DataTable dt = new DataTable();
+
+            // Crear columnas
+            dt.Columns.Add("Property");
+            dt.Columns.Add("Value");
+
+            // Llenar datos
+            foreach (JProperty prop in jsonObject.Properties())
+            {
+                DataRow row = dt.NewRow();
+                row["Property"] = prop.Name;
+                row["Value"] = prop.Value.ToString();
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        }
+
     }
 }
